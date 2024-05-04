95d7b133d160d27a5ec0b1980efebd6cc2b0e5ee
Akinlolu Ogunleye <akinloludavid2727@gmail.com>
2024-05-04 20:59:11 +0100

    updatd margin top for hero text

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -10,21 +10,21 @@ export default function HeroSection() {
             />
             <section className='relative z-10'>
                 <Navbar />
-                <div className='xl:px-[140px] px-6 3xl:px-[240px] md:px-[120px] mx-auto mt-[200px] lg:mt-[320px]'>
+                <div className='xl:px-[140px] px-6 3xl:px-[240px] md:px-[120px] mx-auto mt-[300px] md:mt-[350px] lg:mt-[400px]'>
                     <h1 className='text-[32px] md:text-[48px] lg:w-4/5 3xl:text-[68px] font-normal uppercase font-monument text-white leading-[48px]'>
                         The world's first platform for Tokenizing AI blockchain
                         projects
                     </h1>
 
                     <div className='mt-[40px]'>
-                        <div className='h-[1px] bg-gradient-to-r from-[#3D8BFF] via-[#AB23FF] to-[#AB23FF] md:flex hidden w-[660px]' />
+                        <div className='h-[1px] bg-gradient-to-r from-[#3D8BFF] via-[#AB23FF] to-[#AB23FF] lg:flex hidden w-[660px]' />
                         <div className='bg-gradient-to-r my-1 from-[#3D8BFF] via-[#AB23FF] to-[#AB23FF] text-transparent bg-clip-text'>
                             <p className='font-bold text-[22px] font-satoshi_bold'>
                                 Hold the Creon Pass NFT and earn passive income
                                 from AI Tools
                             </p>
                         </div>
-                        <div className='h-[1px] bg-gradient-to-r from-[#3D8BFF] via-[#AB23FF] to-[#AB23FF] md:flex hidden w-[660px]' />
+                        <div className='h-[1px] bg-gradient-to-r from-[#3D8BFF] via-[#AB23FF] to-[#AB23FF] lg:flex hidden w-[660px]' />
                     </div>
                 </div>
             </section>
